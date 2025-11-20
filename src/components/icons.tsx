8118667c924087300be6e8382915ca356c42dfc6
Pedro Santorio <pedrohsantorio@gmail.com>
2025-11-20 02:01:51 +0000

    Quero que você NÃO gere SVG, NÃO gere vetores, NÃO gere ícones, NÃO rede

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -55,7 +55,7 @@ export function ForbiddenIcon(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="0 0 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
